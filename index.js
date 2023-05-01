3648e1bbaf65a01aeb5adfbf8a2ad01f67acfee4
Mostafiz1257 <mostafiz1257@gmail.com>
2023-05-01 23:08:22 +0600

    added data json file

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const express =require('express');
+const express = require('express');
 const app = express();
 const port = 5000;
+const allData = require('./data/data.json')
+app.get('/', (req, res) => {
+    res.send('Chef cooking')
+})
+app.get('/allData', (req, res) => {
+    res.send(allData)
+})
+
+
+app.listen(port, () => {
+    console.log(`Chef cooking on port ${port}`);
+})
