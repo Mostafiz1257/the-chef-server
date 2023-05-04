95695143821ca4a627ee31b2a2998cfd64d9ffa0
Mostafiz1257 <mostafiz1257@gmail.com>
2023-05-04 17:37:53 +0600

    make link in vercel

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const express = require('express');
-const cors = require('cors')
 const app = express();
+const cors = require('cors')
+const port =process.env.PORT || 5000;
+const allData = require('./data/data.json');
 app.use(cors());
-const port = 5000;
-const allData = require('./data/data.json')
+
 app.get('/', (req, res) => {
     res.send('Chef cooking')
-})
+});
 app.get('/allData', (req, res) => {
     res.send(allData)
 })
 
 app.get('/allData/:id', (req, res) => {
-    const id = req.params.id;
+    const id = parseInt(req.params.id);
     console.log(id);
-    const dataDetails = allData?.find(pd => pd.id ==id)
+    const dataDetails = allData.find(pd => parseInt(pd.id) ===id)
     res.send(dataDetails)
+  
 })
 
 app.listen(port, () => {
-    console.log(`Chef cooking on port ${port}`);
+    console.log(`Chef cooking on port ${port}`)
+    
 })
